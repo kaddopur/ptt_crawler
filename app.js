fcc299b5789b07337b11b9923b77ef7aae34789d
Jason Huang <cjhuang@oath.com>
2019-03-30 15:23:00 +0800

    execute job periodically

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 
 const PTT_PREFIX = 'https://www.ptt.cc';
 const E_SHOPPING_INDEX = 'https://www.ptt.cc/bbs/e-shopping/index.html';
+const TASK_INTERVAL = 3000;
+
+let lastCheck = new Date('1990-01-01');
 
 function processCaseOne(title, url) {
     if (title.indexOf('公告') >= 0) {
@@ -23,13 +26,20 @@ function processCaseOne(title, url) {
             $(datetimeSelector)
                 .last()
                 .text()
-        ).toISOString();
+        );
+
+        // dedupe using datetime
+        if (datetime - lastCheck <= 0) {
+            return;
+        } else {
+            lastCheck = datetime;
+        }
 
         stringify(
             [
                 {
                     title,
-                    datetime
+                    datetime: datetime.toISOString()
                 }
             ],
             {},
@@ -43,16 +53,22 @@ function processCaseOne(title, url) {
     // dedupe
 }
 
-axios.get(E_SHOPPING_INDEX).then(response => {
-    const data = response.data;
-    const $ = cheerio.load(data);
-    const titleLinkSelector = '.r-ent .title a';
+function main() {
+    axios.get(E_SHOPPING_INDEX).then(response => {
+        const data = response.data;
+        const $ = cheerio.load(data);
+        const titleLinkSelector = '.r-ent .title a';
 
-    $(titleLinkSelector).each((index, element) => {
-        const $ele = $(element);
-        const title = $ele.text();
-        const url = $ele.attr('href');
+        $(titleLinkSelector).each((index, element) => {
+            const $ele = $(element);
+            const title = $ele.text();
+            const url = $ele.attr('href');
 
-        processCaseOne(title, url);
+            processCaseOne(title, url);
+        });
     });
-});
+}
+
+setInterval(() => {
+    main();
+}, TASK_INTERVAL);
